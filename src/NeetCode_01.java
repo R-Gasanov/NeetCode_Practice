aef3c6aae8b2190196f71ecc1f6bc2cb2453ce65
Rafael <78911513+R-Gasanov@users.noreply.github.com>
2025-01-06 18:16:03 +0000

    0.1 - Dynamic Arrays (Correct Solution)
    A dynamic array correct solution

--- src/NeetCode_01.java
+++ src/NeetCode_01.java
@@ -1,51 +1,72 @@
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 class DynamicArray {
 	
-	public static void main(String args[])
-	{
-
-	}
-	
+	public int DynmArray[]; // We are declaring an arraylist to be used throughout the applcation
+	public int given_capacity;
+	public int current_size = 0;
 
     public DynamicArray(int capacity) 
     {
         if (capacity > 0)
-        { // Below we are assigining a variable here based on the size (capacity)
-            ArrayList<Integer> DynmArray = new ArrayList<Integer>(capacity);       
+        { 
+        	given_capacity = capacity; // Here, we are storing the size of the capacity
+            DynmArray = new int[capacity]; // Here we are assigning the capacity      
         }
     }
 
     public int get(int i) 
     {
-    	
+    	return DynmArray[i];
+		
     }
 
     public void set(int i, int n) 
     {
-
+    	DynmArray[i] = n; // Here we are assigning the specific element to a new value
     }
 
     public void pushback(int n) 
     {
-
+    	if (current_size == given_capacity)
+    	{
+    		resize();
+    	}
+    	
+    	DynmArray[current_size] = n;
+    	current_size++;
+    	
+ 
     }
 
-    public int popback() {
-
+    public int popback() 
+    {
+    	if (current_size > 0)
+    	{
+    		current_size--;
+    	}
+    	return DynmArray[current_size];
     }
 
-    private void resize() {
-
+    private void resize() 
+    {
+        given_capacity *= 2;
+        int[] doubledArray = new int[given_capacity];
+        for (int index = 0; index < current_size; index++)
+        {
+            doubledArray[index] = DynmArray[index];
+        }
+    	DynmArray = doubledArray;
     }
 
     public int getSize() 
     {
-        
+        return current_size;
     }
 
-    public int getCapacity() {
-
+    public int getCapacity() 
+    { // We made a global variable where it'll essentially be used to show the capacity provided
+    	return given_capacity;
     }
 }
