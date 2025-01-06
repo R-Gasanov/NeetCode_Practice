b582aaa919cbfc91162e6a5bf625a979a42534e1
Rafael <78911513+R-Gasanov@users.noreply.github.com>
2025-01-06 16:35:05 +0000

    Initial commit

---
+++ src/NeetCode_01.java
@@ -0,0 +1,51 @@
+
+import java.util.ArrayList;
+
+class DynamicArray {
+	
+	public static void main(String args[])
+	{
+
+	}
+	
+
+    public DynamicArray(int capacity) 
+    {
+        if (capacity > 0)
+        { // Below we are assigining a variable here based on the size (capacity)
+            ArrayList<Integer> DynmArray = new ArrayList<Integer>(capacity);       
+        }
+    }
+
+    public int get(int i) 
+    {
+    	
+    }
+
+    public void set(int i, int n) 
+    {
+
+    }
+
+    public void pushback(int n) 
+    {
+
+    }
+
+    public int popback() {
+
+    }
+
+    private void resize() {
+
+    }
+
+    public int getSize() 
+    {
+        
+    }
+
+    public int getCapacity() {
+
+    }
+}
